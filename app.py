a556f2b41d4991d9fcc20c160e43afed1ab1915a
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2021-01-26 15:52:43 +0200

    Implemented user login and creation succesfully

--- app.py
+++ app.py
@@ -20,13 +20,28 @@ def index():
     cars = result.fetchall()
     return render_template("index.html", cars=cars)
 
-@app.route("/login", methods=["POST"])
-def login():
+@app.route("/login_user", methods=["POST"])
+def login_as_user():
     username = request.form["username"]
     password = request.form["password"]
-    #TODO: check username and password
-    session["username"] = username
-    return redirect("/")
+    sql = "SELECT password, id FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if user == None:
+        return render_template("no_user.html", user=user)
+    else:
+        if check_password_hash(user[0], password):
+            session["user_id"] = user[1]
+            session["username"] = username
+            return redirect("/")
+
+
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.html")
+
+def user_id():
+    return session.get("user_id", 0)
 
 @app.route("/new")
 def new_car_form():
@@ -69,13 +84,6 @@ def send():
     engine = request.form["engine"]
     power = request.form["power"]
     legal = request.form["legal"]
-    print(legal)
-    #sql = "INSERT INTO cars (brand, model, chassis, fuel, " \
-    #      "drive, transmission, mileage, year, price, color, " \
-    #      "engine, power, legal, info) VALUES (:brand, :model, " \
-    #      ":chassis, :fuel, :drive, :transmission, :mileage, :year, " \
-    #      ":price, :color, :engine, :power, :legal)"
-
     sql = "INSERT INTO cars (brand, model, chassis, fuel, drive, transmission, mileage, year, price, color, engine, power, street_legal) VALUES (:brand, :model, :chassis, :fuel, :drive, :transmission, :mileage, :year, :price, :color, :engine, :power, :street_legal)"
     db.session.execute(sql, {"brand":brand, "model":model, "chassis":chassis,
     "fuel":fuel, "drive":drive, "transmission":transmission, "mileage":mileage,
@@ -86,9 +94,27 @@ def send():
 
 @app.route("/logout")
 def logout():
-    del session["username"]
+    del session["user_id"]
     return redirect("/")
 
 @app.route("/register", methods=["GET","POST"])
 def register():
-    return redirect("/")
+    return render_template("user_form.html")
+
+@app.route("/new_user", methods=["GET","POST"])
+def create_new_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    first_name = request.form["fname"]
+    last_name = request.form["sname"]
+    location = request.form["location"]
+    phone = request.form["tel"]
+    email = request.form["email"]
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO users (username, firstname, surname, telephone, email, location, admin, password) VALUES (:username, :firstname, :surname, :telephone, :email, :location, :admin, :password)"
+        db.session.execute(sql, {"username":username,"firstname":first_name,"surname":last_name,"telephone":phone,"email":email, "location":location, "admin":False, "password":hash_value})
+        db.session.commit()
+        return redirect("/")
+    except:
+        return render_template("error.html")
